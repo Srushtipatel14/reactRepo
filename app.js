5313c293816e79943038a1a4784bc8ae0ffa5d4d
srushti patel <srushtip579@gmail.com>
2024-07-30 22:45:13 +0530

    commit by situ

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client"
 
 export const App=()=>{
     return (
+       <>
+         <div>
+            <h1>This is Heading Tag H1</h1>
+            <h2>This is Heading Tag H2</h2>
+            <h3>This is Heading Tag H3</h3>
+            <h4>This is Heading Tag H4</h4>
+            <h5>This is Heading Tag H5</h5>
+            <h6>This is Heading Tag H6</h6>
+        </div>
         <div>
             <h1>This is Heading Tag H1</h1>
             <h2>This is Heading Tag H2</h2>
@@ -11,6 +20,7 @@ export const App=()=>{
             <h5>This is Heading Tag H5</h5>
             <h6>This is Heading Tag H6</h6>
         </div>
+       </>
     )
 }
 
